29bb82dd9e2f13168ff478a4677e24e8a2ed6d7b
alihusains <123808017+alihusains@users.noreply.github.com>
2024-01-07 13:07:20 +0000

    Update api_script.py

--- .github/scripts/api_script.py
+++ .github/scripts/api_script.py
@@ -1,3 +1,4 @@
+import os
 import requests
 from datetime import datetime
 
@@ -5,19 +6,22 @@ from datetime import datetime
 API_URL = 'https://script.google.com/macros/s/AKfycbyrh3wEuvmOlCu2iZaA393m8WMUZHc1555RqtQp6dg4GCy_xTA936tvhydSt864VblCJg/exec?sheet=raw&action=read'
 
 # Define the file format
-file_format = 'data_%d-%m-%Y.txt'
+file_format = 'daa_%d-%m-%Y.json'
 
 # Create a timestamp for the current time
 timestamp = datetime.now().strftime(file_format)
+
+# Specify the full path where the file should be saved
+file_path = os.path.join(os.environ['GITHUB_WORKSPACE'], f'{timestamp}.json')
 
 # Make API call
 response = requests.get(API_URL)
 
 # Check if the API call was successful (status code 200)
 if response.status_code == 200:
-    # Save the API response to a file
-    with open(timestamp, 'w') as file:
+    # Save the API response to the specified file path
+    with open(file_path, 'w') as file:
         file.write(response.text)
-    print(f"API response saved to {timestamp}")
+    print(f"API response saved to {file_path}")
 else:
     print(f"Failed to fetch API data. Status code: {response.status_code}")
